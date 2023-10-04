59e9a1fe80af0a88f6f794f1f5c684595b4751df
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-10-04 15:32:40 -0300

    exercicio de comentarios

---
+++ comentario.go
@@ -0,0 +1,14 @@
+package exercicios
+/* Package weather provides functionality to retrieve and display information about current weather conditions. */
+
+// CurrentCondition stores the current weather condition.
+var CurrentCondition string
+
+// CurrentLocation stores the current location for weather information.
+var CurrentLocation string
+
+/* Forecast function forecasts the current weather condition for a given city. */
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
