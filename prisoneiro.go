415538c6a3f6f071038bf3d3fe435b9c6638ced9
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-10-06 18:11:32 -0300

    exercicio prisioneiro resolvido

--- prisoneiro.go
+++ prisoneiro.go
@@ -1,22 +1,22 @@
 package exercicios
 
-// CanQuickAttack pode ser executado apenas quando o cavaleiro está dormindo, pois ele está vulnerável enquanto coloca a armadura.
+// CanFastAttack só pode ser executada quando o cavaleiro está dormindo, pois ele está vulnerável enquanto coloca a armadura.
 func CanFastAttack(knightIsAwake bool) bool {
 	return !knightIsAwake
 }
 
-// CanSpy pode ser executado se pelo menos um dos personagens estiver acordado.
+// CanSpy pode ser executada se pelo menos um dos personagens estiver acordado.
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
 	return knightIsAwake || archerIsAwake || prisonerIsAwake
 }
 
-// CanSignalPrisoner pode ser executado se o arqueiro estiver acordado e o prisioneiro estiver acordado.
+// CanSignalPrisoner pode ser executada se o arqueiro estiver acordado e o prisioneiro estiver acordado.
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	return archerIsAwake && prisonerIsAwake
+	return !archerIsAwake && prisonerIsAwake
 }
 
-// CanReleasePrisoner pode ser executado se o prisioneiro estiver acordado e os outros 2 personagens estiverem dormindo
-// ou se o cachorro de Annalyn estiver com ela e o arqueiro estiver dormindo.
+// CanFreePrisoner pode ser executada se o prisioneiro estiver acordado e os outros 2 personagens estiverem dormindo
+// ou se o cachorro de Annalyn estiver presente e o arqueiro estiver dormindo.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, annalynsDogIsPresent bool) bool {
 	if annalynsDogIsPresent {
 		return !archerIsAwake
