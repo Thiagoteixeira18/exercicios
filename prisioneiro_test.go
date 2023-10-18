07b1abfdd42d2e5c59354f7e2661cc5c36cd03cd
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-10-18 15:45:15 -0300

    adicionado test no pacote prisioneiro canspy

--- prisioneiro_test.go
+++ prisioneiro_test.go
@@ -4,20 +4,37 @@ import (
 	"testing"
 )
 
-
 func TestCanFastAttack(t *testing.T) {
 	tests := []struct {
 		knightIsAwake bool
 		expected      bool
 	}{
 		{true, false},
-		{false, true}, 
+		{false, true},
 	}
 
 	for _, test := range tests {
 		result := CanFastAttack(test.knightIsAwake)
 		if result != test.expected {
 			t.Errorf("Para knightIsAwake=%v, esperado %v, mas obtido %v", test.knightIsAwake, test.expected, result)
+		}
+	}
+}
+
+func TestCanSpy(t *testing.T) {
+	teste := []struct {
+		knightIsAwake   bool
+		archerIsAwake   bool
+		prisonerIsAwake bool
+		esperado        bool
+	}{
+		{false, false, true, false},
+		{true, true, true, true},
+	}
+	for _, test := range teste {
+		result := (test.knightIsAwake)
+		if result != test.esperado {
+			t.Errorf("Para canspy=%v, esperado %v, mas obtido %v", test.knightIsAwake, test.esperado, result)
 		}
 	}
 }
