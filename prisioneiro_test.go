600e306f12c93f71aa728e157ccc126e03f55ecd
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-10-19 16:24:47 -0300

    adicionado test prisioneiro refeiro

--- prisioneiro_test.go
+++ prisioneiro_test.go
@@ -22,19 +22,66 @@ func TestCanFastAttack(t *testing.T) {
 }
 
 func TestCanSpy(t *testing.T) {
-	teste := []struct {
+	tests := []struct {
 		knightIsAwake   bool
 		archerIsAwake   bool
 		prisonerIsAwake bool
-		esperado        bool
+		expected        bool
 	}{
-		{false, false, true, false},
+		{false, false, true, true},
 		{true, true, true, true},
+		{false, true, false, true},
+		{true, false, false, true},
 	}
-	for _, test := range teste {
-		result := (test.knightIsAwake)
-		if result != test.esperado {
-			t.Errorf("Para canspy=%v, esperado %v, mas obtido %v", test.knightIsAwake, test.esperado, result)
+
+	for _, test := range tests {
+		result := CanSpy(test.knightIsAwake, test.archerIsAwake, test.prisonerIsAwake)
+		if result != test.expected {
+			t.Errorf("Para knightIsAwake=%v, archerIsAwake=%v, prisonerIsAwake=%v, esperado %v, mas obtido %v", test.knightIsAwake, test.archerIsAwake, test.prisonerIsAwake, test.expected, result)
+		}
+	}
+}
+
+func TestCanSignalPrisoner(t *testing.T) {
+	tests := []struct {
+		archerIsAwake   bool
+		prisonerIsAwake bool
+		expected        bool
+	}{
+		{false, true, true},
+		{true, true, false},
+		{false, false, false},
+		{true, false, false},
+	}
+
+	for _, test := range tests {
+		result := CanSignalPrisoner(test.archerIsAwake, test.prisonerIsAwake)
+		if result != test.expected {
+			t.Errorf("Para archerIsAwake=%v, prisonerIsAwake=%v, esperado %v, mas obtido %v", test.archerIsAwake, test.prisonerIsAwake, test.expected, result)
+		}
+	}
+}
+
+func TestCanFreePrisoner(t *testing.T) {
+	tests := []struct {
+		knightIsAwake        bool
+		archerIsAwake        bool
+		prisonerIsAwake      bool
+		annalynsDogIsPresent bool
+		expected             bool
+	}{
+		{true, true, true, true, false},
+		{true, false, true, false, false},
+		{false, true, true, true, false},
+		{false, true, false, true, true},
+		{false, false, true, false, true},
+		{true, true, true, true, false},
+	}
+
+	for _, test := range tests {
+		result := CanFreePrisoner(test.knightIsAwake, test.archerIsAwake, test.prisonerIsAwake, test.annalynsDogIsPresent)
+		if result != test.expected {
+			t.Errorf("Para knightIsAwake=%v, archerIsAwake=%v, prisonerIsAwake=%v, annalynsDogIsPresent=%v, esperado %v, mas obtido %v", test.knightIsAwake, test.archerIsAwake, test.prisonerIsAwake, test.annalynsDogIsPresent, test.expected, result)
 		}
 	}
 }
