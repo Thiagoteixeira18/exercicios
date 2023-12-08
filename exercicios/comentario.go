fc707dd730d8384ada38a7028b7a317f9a37276d
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-12-08 13:47:53 -0300

    exercicio cometarios completo

--- exercicios/comentario.go
+++ exercicios/comentario.go
@@ -1,5 +1,5 @@
 package exercicios
-/* Package weather provides functionality to retrieve and display information about current weather conditions. */
+// Package weather provides functionality to retrieve and display information about current weather conditions. 
 
 // CurrentCondition stores the current weather condition.
 var CurrentCondition string
@@ -7,7 +7,7 @@ var CurrentCondition string
 // CurrentLocation stores the current location for weather information.
 var CurrentLocation string
 
-/* Forecast function forecasts the current weather condition for a given city. */
+// Forecast function forecasts the current weather condition for a given city. 
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
