2fd7a108233aca61f102d3b95035d94b78f99929
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-12-18 19:46:25 -0300

    exercicios closure acresentado int

--- exercicios/clousures/closure.go
+++ exercicios/clousures/closure.go
@@ -4,15 +4,18 @@ import "fmt"
 
 func clouse() func() {
 	texto := "exercicio de closure"
-
+	numero := 50
 	funcao := func() {
 		fmt.Println(texto)
+		fmt.Println(numero)
 	}
 	return funcao
 }
 func main() {
+	numero := 20
 	texto := "exercicio"
 	fmt.Println(texto)
+	fmt.Println(numero)
 
 	funcaonova := clouse()
 	funcaonova()
